a181cb3ecf3ba7aa778dc940ca8614258f1b521d
DouglasSherk <github@sherk.me>
2012-09-05 00:05:15 -0400

    Fix redefinition of function problem with local OSX server

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,13 @@
 | path to your installation.
 |
  */
+
+// drs: FIXME This should be defined elsewhere. This fixes a problem with
+// redefinition of this function, for now.
+if (!defined(is_ssl)) {
 function is_ssl() {
 	return isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off';
+}
 }
 
 $config['base_url']	= '';
