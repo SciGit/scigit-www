f5b85f0f230ab227b974c4b1acb2dab6a33abc5d
DouglasSherk <github@sherk.me>
2012-09-08 02:40:44 -0400

    snap hero unit to the size of the login bar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-/* Author:
+/* Author: SciGit team.
+ * Random utility functions.
+ * drs: XXX comment properly.
+ */
 
-*/
-
-
-
-
-
-
-
+function equalizeColumns() {
+  $('.hero-unit').css({
+      'height': $('.login-form').height() -
+                parseInt($('.hero-unit').css('padding-top')) -
+                parseInt($('.hero-unit').css('padding-bottom'))
+  });
+}
