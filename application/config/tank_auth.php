61232d7f02b7f886999b7028be3eb3349aa32aba
DouglasSherk <github@sherk.me>
2012-09-08 13:37:51 -0400

    adjust captcha time from 24h->1h

--- application/config/tank_auth.php
+++ application/config/tank_auth.php
@@ -74,7 +74,7 @@ $config['login_record_ip'] = TRUE;
 $config['login_record_time'] = TRUE;
 $config['login_count_attempts'] = TRUE;
 $config['login_max_attempts'] = 5;
-$config['login_attempt_expire'] = 60*60*24;
+$config['login_attempt_expire'] = 60*60; /* 1 hour */
 
 /*
 |--------------------------------------------------------------------------
