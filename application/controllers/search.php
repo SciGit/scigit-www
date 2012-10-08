94a0e20fa0cb5f1718bd6278176fb2303c289d16
DouglasSherk <github@sherk.me>
2012-10-08 16:37:27 -0400

    properly write timestamp on search results

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -8,6 +8,7 @@ class Search extends CI_Controller
 		$this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
     $this->load->model('project');
+    $this->load->model('change');
 		$this->form_validation->set_error_delimiters('', '');
   }
 
@@ -26,6 +27,15 @@ class Search extends CI_Controller
       }
 
       foreach ($projects as $project) {
+        $latest_change = $this->change->get_by_project_latest($project->id, 0, 1);
+        if ($latest_change && $latest_change[0]) {
+          $project->latest_change = $latest_change[0]->commit_ts;
+        } else {
+          // The project was just created and has no changes. Mark its creation
+          // date instead.
+          $project->latest_change = $project->created_ts;
+        }
+
         $highlight_start = '<span style="color:#990000">';
         $highlight_end = '</span>';
         foreach ($words as $word) {
