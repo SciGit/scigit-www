d86b2d46a6e594586e29c5cb3f59ec68f9d98f43
DouglasSherk <github@sherk.me>
2012-10-08 15:50:37 -0400

    working searching, highlights and searches word-by-word, prioritizing matches with project names rather than descriptions

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -13,13 +13,31 @@ class Search extends CI_Controller
 
   public function index()
   {
-    $this->form_validation->set_rules('search', 'Search', 'xss_clean');
+    $this->form_validation->set_rules('search', 'Search', 'xss_clean|trim');
 
     if ($this->form_validation->run()) {
       $query = $this->input->post('search');
 
+      $projects = $this->project->search_by_name_and_desc($query);
+
+      $words = explode(' ', $query);
+      foreach ($words as $word) {
+        $projects += $this->project->search_by_name_and_desc($word);
+      }
+
+      foreach ($projects as $project) {
+        $highlight_start = '<span style="color:#990000">';
+        $highlight_end = '</span>';
+        foreach ($words as $word) {
+          $project->name = highlight_phrase($project->name, $word,
+            $highlight_start, $highlight_end);
+          $project->description = highlight_phrase($project->description, $word,
+            $highlight_start, $highlight_end);
+        }
+      }
+
       $data = array(
-        'results' => $this->project->search_by_name_and_desc($query),
+        'results' => $projects,
       );
 
       $this->twig->display('search.twig', $data);
