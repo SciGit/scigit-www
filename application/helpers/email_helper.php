73e5b6ea821d07410e784647caf17be3d57a9470
DouglasSherk <github@sherk.me>
2012-10-15 00:24:13 -0400

    clean up email message sent

--- application/helpers/email_helper.php
+++ application/helpers/email_helper.php
@@ -6,15 +6,15 @@ function email_project_update($change_id, $to) {
   $project = $CI->project->get($change->proj_id);
   $user = $CI->user->get_user_by_id($change->user_id, true);
 
-  $CI->postageapp->from('test@scigit.com');
+  $CI->postageapp->from('no-reply@scigit.com');
   $CI->postageapp->to($to->email);
-  $CI->postageapp->subject('Test!');
-  $CI->postageapp->message('Test message');
+  $CI->postageapp->subject("$project->name change");
+  $CI->postageapp->message(
+    "$user->username has made a change to $project->name. \"$change->commit_msg\"");
 
   $CI->postageapp->template('project_update');
   $CI->postageapp->variables(array(
-    //'site' => 'http://scigit.com',
-    'site' => 'http://localhost',
+    'site' => 'http://scigit.com',
     'user_id' => $user->id,
     'user_name' => $user->username,
     'proj_id' => $project->id,
@@ -23,6 +23,4 @@ function email_project_update($change_id, $to) {
     'commit_msg' => $change->commit_msg,
     'commit_ts' => date("H:i d/m/Y", $change->commit_ts)
   ));
-
-  var_dump($CI->postageapp->send());
 }
