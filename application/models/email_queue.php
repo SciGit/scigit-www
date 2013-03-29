b3be33048b00acaab20f2e710317341164bc4d75
DouglasSherk <github@sherk.me>
2013-03-29 15:31:41 -0400

    better delete member confirmation

--- application/models/email_queue.php
+++ application/models/email_queue.php
@@ -5,6 +5,7 @@ class Email_Queue extends CI_Model
   public $email_table = 'email_queue';
   public $email_user_table = 'email_user_queue';
   public $email_add_to_project_table = 'email_add_to_project_queue';
+  public $email_invite_to_scigit_table = 'email_invite_to_scigit_queue';
 
   public function get() {
     return $this->db->get($this->email_table)->result();
@@ -18,6 +19,10 @@ class Email_Queue extends CI_Model
     return $this->db->get($this->email_add_to_project_table)->result();
   }
 
+  public function get_invite_to_scigit_emails() {
+    return $this->db->get($this->email_invite_to_scigit_table)->result();
+  }
+
   public function clear() {
     return $this->db->empty_table($this->email_table);
   }
@@ -28,6 +33,10 @@ class Email_Queue extends CI_Model
 
   public function clear_add_to_project_emails() {
     return $this->db->empty_table($this->email_add_to_project_table);
+  }
+
+  public function clear_invite_to_scigit_emails() {
+    return $this->db->empty_table($this->email_invite_to_scigit_table);
   }
 
   public function add_user_email($user_id) {
