8d8859610f490b5478c0ac5f7f9fe0bbeb93b325
Hanson Wang <hanson.wng@gmail.com>
2013-03-30 20:15:04 +0000

    Fix git diff syntax

--- application/helpers/scigit_helper.php
+++ application/helpers/scigit_helper.php
@@ -144,7 +144,7 @@ function scigit_get_listing($proj_id, $commit_hash, $path) {
 function scigit_get_diff($proj_id, $commit_hash, $path) {
   $dir = SCIGIT_REPO_DIR . '/r' . $proj_id;
   $escape_path = escapeshellarg($path);
-	$handle = popen("cd $dir; git diff $commit_hash:$escape_path", 'r');
+	$handle = popen("cd $dir; git diff $commit_hash^ $commit_hash -- $escape_path", 'r');
 	$output = '';
 	while (!feof($handle)) {
 		$output .= fread($handle, 1024);
