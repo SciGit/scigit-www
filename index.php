71f739b8bbbe77db17ed5ca536f7e34b457999fc
root <root@domU-12-31-39-17-2A-7B.compute-1.internal>
2013-04-01 19:48:42 +0000

    add environment handling

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+include 'env.php';
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
