1ab1b67c69de6204e651586ca3ad380992096241
Hanson Wang <hanson.wng@gmail.com>
2013-04-02 02:51:33 +0000

    Change website client version to 1.0.0.0

--- application/controllers/api/client_version.php
+++ application/controllers/api/client_version.php
@@ -5,6 +5,6 @@ require APPPATH.'/core/SciGit_REST_Controller.php';
 class Client_version extends SciGit_REST_Controller
 {
 	public function index_get() {
-		$this->response(array('version' => '0.1.0.0'));
+		$this->response(array('version' => '1.0.0.0'));
 	}
 }
