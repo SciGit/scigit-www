8819f8e18ec09e35f333367fdf6a1a4aa1a2fdf2
DouglasSherk <github@sherk.me>
2013-04-03 16:27:22 -0400

    put proper hostname on git clone command on project view

--- application/helpers/environment_helper.php
+++ application/helpers/environment_helper.php
@@ -1,11 +1,16 @@
 <?
 
 define('SCIGIT_BASE_PATH_PRE', 'https://');
+define('SCIGIT_BASE_PATH_PRE_GIT', 'ssh://git@');
 define('SCIGIT_BASE_PATH_POST', 'scigit.com');
 
-define('SCIGIT_DEVELOPMENT_PATH', 'localhost');
+define('SCIGIT_DEVELOPMENT_PATH', SCIGIT_BASE_PATH_PRE + 'localhost');
 define('SCIGIT_TESTING_PATH', SCIGIT_BASE_PATH_PRE . 'stage.' . SCIGIT_BASE_PATH_POST);
 define('SCIGIT_PRODUCTION_PATH', SCIGIT_BASE_PATH_PRE . 'www.' . SCIGIT_BASE_PATH_POST);
+
+define('SCIGIT_DEVELOPMENT_PATH_GIT', SCIGIT_BASE_PATH_PRE_GIT . 'localhost');
+define('SCIGIT_TESTING_PATH_GIT', SCIGIT_BASE_PATH_PRE_GIT . 'stage.' . SCIGIT_BASE_PATH_POST);
+define('SCIGIT_PRODUCTION_PATH_GIT', SCIGIT_BASE_PATH_PRE_GIT . 'www.' . SCIGIT_BASE_PATH_POST);
 
 function get_hostname() {
   switch (ENVIRONMENT) {
@@ -15,6 +20,17 @@ function get_hostname() {
       return SCIGIT_TESTING_PATH;
     case 'production':
       return SCIGIT_PRODUCTION_PATH;
+  }
+}
+
+function get_git_path() {
+  switch (ENVIRONMENT) {
+    case 'development':
+      return SCIGIT_DEVELOPMENT_PATH_GIT;
+    case 'testing':
+      return SCIGIT_TESTING_PATH_GIT;
+    case 'production':
+      return SCIGIT_PRODUCTION_PATH_GIT;
   }
 }
 
