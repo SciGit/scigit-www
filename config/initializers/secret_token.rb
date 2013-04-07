1de2ddb159b5ca637393ecbcff262f1d1bd03742
DouglasSherk <github@sherk.me>
2013-04-07 16:48:33 -0400

    Upgrade Rails and base gems.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ScigitWww::Application.config.secret_token = '93f6ea4a9048db405ccf4ff45334d68bfd654c1f718cf359016cf37bb96ca98ab0ac2122c230e3b8336db4e75f245a7ba5dfc3b2dbb788ff31886b4db4be3c79'
+ScigitWww::Application.config.secret_token = '7649ba0093ef3f898990d8174622a9c5ab442414c45f6ee09e2aa78f64a5c2620b7aef65cf4752a4f4bb6a61ddfce1d360edbd9670607ca422c562e7181ebfc0'
