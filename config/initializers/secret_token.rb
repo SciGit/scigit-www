ad740cd8fa7ea44d15832939b9cefaeb929307c7
DouglasSherk <github@sherk.me>
2013-04-07 16:38:28 -0400

    Beginning of Rails port.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScigitWww::Application.config.secret_token = '93f6ea4a9048db405ccf4ff45334d68bfd654c1f718cf359016cf37bb96ca98ab0ac2122c230e3b8336db4e75f245a7ba5dfc3b2dbb788ff31886b4db4be3c79'
