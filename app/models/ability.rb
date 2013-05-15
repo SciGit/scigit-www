e3c7212a43fe09fed90a8ab9bf90e6c7c87a1ba6
DouglasSherk <github@sherk.me>
2013-05-15 17:54:06 -0400

    Properly load and store permissions from database; basic handling.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,27 @@
 class Ability
   include CanCan::Ability
 
+  def user_has_permission(user, project, minimum)
+    permission = ProjectPermission.get_user_permission(user, project)
+    return false if permission.nil?
+    return true if permission.permission >= minimum
+  end
+
   def initialize(user)
+    user ||= User.new
+
+    can :manage, Project, Project do |project|
+      user_has_permission(user, project, 3)
+    end
+
+    can :update, Project, Project do |project|
+      user_has_permission(user, project, 2)
+    end
+
+    can :read, Project, Project do |project|
+      user_has_permission(user, project, 1)
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- app/models/project_permission.rb
+++ app/models/project_permission.rb
@@ -1,4 +1,12 @@
 class ProjectPermission < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  MANAGE = 3
+  UPDATE = 2
+  READ = 1
+
+  def self.get_user_permission(user, project)
+    where(:user_id => user[:id], :project_id => project[:id]).first
+  end
 end
