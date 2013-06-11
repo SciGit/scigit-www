a04d0b6d8e62b98f8ff09d1d2ce8d92e39ba9056
DouglasSherk <github@sherk.me>
2013-06-11 00:29:39 +0000

    Update everything so it can run in production.

--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(version: 20130531235031) do
   add_index "user_public_keys", ["user_id"], name: "index_user_public_keys_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -66,7 +66,9 @@ ActiveRecord::Schema.define(version: 20130531235031) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",                    default: 0
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0
+    t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
