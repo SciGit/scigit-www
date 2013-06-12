9ee27d2f1fe70bfe7760d535c2ab16dfcdd77488
DouglasSherk <github@sherk.me>
2013-06-12 12:16:10 -0400

    Fix deploy script and database config for testing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,38 +17,11 @@ set :use_sudo, false
 
 set :normalize_asset_timestamps, false
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, production # Your HTTP server, Apache/etc
-#role :app, production # This may be the same as your `Web` server
-#role :db,  production, :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-#
-
-task :uname do
-  run "uname -a"
-end
-
 task :deploy_scripts do
   run "sudo /etc/init.d/scigit stop &&
        cd /var/scigit &&
        git pull &&
+       sudo chown -hR git:deploy /var/scigit &&
        sudo /etc/init.d/scigit start"
 end
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 set :rails_env, "test"
 
-server 'ec2-54-226-74-157.compute-1.amazonaws.com', :app, :web, :db, :primary => true
+server 'ec2-23-22-218-189.compute-1.amazonaws.com', :app, :web, :db, :primary => true
