8f1c7f4d9ba9a08a4c562255f8fff23473ecaaca
DouglasSherk <github@sherk.me>
2013-06-12 18:07:08 -0400

    Update deployment scripts to point to actual scigit.com DNS instead of AWS.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :rails_env, "production"
 
-set :server1, 'ec2-54-226-202-69.compute-1.amazonaws.com'
+set :server1, 'rails.scigit.com'
 #set :server2, 'ec2-54-224-24-151.compute-1.amazonaws.com'
 
 #server 'ec2-23-22-7-52.compute-1.amazonaws.com', :app, :web, :db, :primary => true
 role :app, server1#, server2
 role :web, server1#, server2
-role :db, server1#, server2, :primary => true
+role :db, server1, :primary => true #, server2, :primary => true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 set :rails_env, "production"
 
-server 'ec2-23-22-255-175.compute-1.amazonaws.com', :app, :web, :db, :primary => true
+server 'rails-staging.scigit.com', :app, :web, :db, :primary => true
