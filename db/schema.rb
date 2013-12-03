379417e1b909d2d4deac3dcc14246c7c622406aa
DouglasSherk <github@sherk.me>
2013-12-03 16:43:51 -0500

    Use scigit user instead of root in development.

--- db/schema.rb
+++ db/schema.rb
@@ -13,80 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130613185145) do
 
-  create_table "ci_sessions", primary_key: "session_id", force: true do |t|
-    t.string  "ip_address",    limit: 16,  default: "0", null: false
-    t.string  "user_agent",    limit: 150,               null: false
-    t.integer "last_activity",             default: 0,   null: false
-    t.text    "user_data",                               null: false
-  end
-
-  create_table "email_add_to_project_queue", force: true do |t|
-    t.integer "user_id",  null: false
-    t.integer "user_id2", null: false
-    t.integer "proj_id",  null: false
-  end
-
-  create_table "email_invite_to_scigit_queue", force: true do |t|
-    t.integer "user_id",                null: false
-    t.integer "proj_id",                null: false
-    t.string  "to",         limit: 100, null: false
-    t.integer "permission",             null: false
-    t.string  "hash",       limit: 64,  null: false
-  end
-
-  add_index "email_invite_to_scigit_queue", ["proj_id"], name: "proj_id", using: :btree
-  add_index "email_invite_to_scigit_queue", ["user_id"], name: "user_id", using: :btree
-
-  create_table "email_queue", force: true do |t|
-    t.integer "change_id", null: false
-  end
-
-  add_index "email_queue", ["change_id"], name: "change_id", using: :btree
-
-  create_table "email_user_queue", force: true do |t|
-    t.integer "user_id", null: false
-  end
-
-  add_index "email_user_queue", ["user_id"], name: "user_id", using: :btree
-
-  create_table "login_attempts", force: true do |t|
-    t.string    "ip_address", limit: 40, null: false
-    t.string    "login",      limit: 50, null: false
-    t.timestamp "time",                  null: false
-  end
-
-  create_table "migrations", id: false, force: true do |t|
-    t.integer "version", null: false
-  end
-
-  create_table "proj_changes", force: true do |t|
-    t.integer "proj_id",     null: false
-    t.integer "user_id",     null: false
-    t.string  "commit_msg",  null: false
-    t.string  "commit_hash", null: false
-    t.integer "commit_ts",   null: false
-  end
-
-  add_index "proj_changes", ["proj_id"], name: "pc_proj_index", using: :btree
-
-  create_table "proj_permissions", force: true do |t|
-    t.integer "user_id",    null: false
-    t.integer "proj_id",    null: false
-    t.integer "permission", null: false
-  end
-
-  add_index "proj_permissions", ["proj_id"], name: "proj_id", using: :btree
-  add_index "proj_permissions", ["user_id"], name: "user_id", using: :btree
-
-  create_table "proj_permissions_temp", force: true do |t|
-    t.integer "user_id",    null: false
-    t.integer "proj_id",    null: false
-    t.integer "permission", null: false
-  end
-
-  add_index "proj_permissions_temp", ["proj_id"], name: "proj_id", using: :btree
-  add_index "proj_permissions_temp", ["user_id"], name: "user_id", using: :btree
-
   create_table "project_changes", force: true do |t|
     t.integer  "user_id"
     t.integer  "project_id"
@@ -113,53 +39,6 @@ ActiveRecord::Schema.define(version: 20130613185145) do
     t.datetime "updated_at"
   end
 
-  create_table "user_auth_tokens", force: true do |t|
-    t.integer "user_id",    null: false
-    t.string  "auth_token", null: false
-    t.string  "ip_address", null: false
-    t.integer "created_ts", null: false
-    t.integer "expiry_ts",  null: false
-  end
-
-  add_index "user_auth_tokens", ["expiry_ts"], name: "token_expiry", using: :btree
-  add_index "user_auth_tokens", ["user_id", "auth_token"], name: "token_index", unique: true, using: :btree
-
-  create_table "user_autologin", id: false, force: true do |t|
-    t.string    "key_id",     limit: 32,              null: false
-    t.integer   "user_id",                default: 0, null: false
-    t.string    "user_agent", limit: 150,             null: false
-    t.string    "last_ip",    limit: 40,              null: false
-    t.timestamp "last_login",                         null: false
-  end
-
-  create_table "user_invites", force: true do |t|
-    t.integer "proj_id",                null: false
-    t.string  "to",         limit: 100, null: false
-    t.integer "permission",             null: false
-    t.string  "hash",       limit: 64,  null: false
-  end
-
-  add_index "user_invites", ["proj_id"], name: "proj_id", using: :btree
-
-  create_table "user_profiles", force: true do |t|
-    t.integer "user_id",            null: false
-    t.string  "country", limit: 20
-    t.string  "website"
-  end
-
-  create_table "user_pub_keys", force: true do |t|
-    t.integer "user_id",                                null: false
-    t.string  "name",                                   null: false
-    t.string  "key_type",                               null: false
-    t.string  "public_key",     limit: 512,             null: false
-    t.string  "comment",                                null: false
-    t.integer "auto_generated",             default: 1, null: false
-    t.integer "enabled",                    default: 1, null: false
-  end
-
-  add_index "user_pub_keys", ["public_key"], name: "pk_key_index", unique: true, using: :btree
-  add_index "user_pub_keys", ["user_id"], name: "pk_user_index", using: :btree
-
   create_table "user_public_keys", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -172,15 +51,6 @@ ActiveRecord::Schema.define(version: 20130613185145) do
   end
 
   add_index "user_public_keys", ["user_id"], name: "index_user_public_keys_on_user_id", using: :btree
-
-  create_table "user_seen", force: true do |t|
-    t.integer "user_id",             null: false
-    t.string  "key",     limit: 100, null: false
-    t.integer "value",               null: false
-  end
-
-  add_index "user_seen", ["key"], name: "key", using: :btree
-  add_index "user_seen", ["user_id"], name: "user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
