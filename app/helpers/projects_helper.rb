f42891fa58353e7f5d5c0b1f9c0ceddd69c28033
hanson.wang <hanson.wng@gmail.com>
2013-12-12 02:09:24 +0000

    Breadcrumb paths in file listing

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,22 @@
 module ProjectsHelper
+  def path_breadcrumb(path, project, change)
+    result = ""
+    if path.blank?
+      result += 'Project Files'
+    else
+      result += link_to('Project Files', files_project_path(project, :change => change.id))
+    end
+
+    parts = (path || '').split('/')
+    parts.each_with_index do |part, i| 
+      result += " / "
+      if i != parts.length - 1
+        result += link_to(part, files_project_path(project, {:path => parts[0..i].join('/'), :change => change.id}))
+      else
+        result += part
+      end
+    end
+
+    result
+  end
 end
